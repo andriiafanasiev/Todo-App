e0cd8022b0c47a395abe10a40484f4e95ef89c82
andriiafanasiev <andreynoobroo@gmail.com>
2024-09-21 22:41:09 +0200

    Better styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <h1 style={{ marginTop: '10%' }}>Todo App</h1>
       <TodoForm todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} />
     </div>
--- src/components/Todos/TodoList.jsx
+++ src/components/Todos/TodoList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Todo from './Todo';
+import styles from './TodoList.module.css';
 
 function TodoList({ todos }) {
   return (
-    <div>
+    <div className={styles.todo__todolist}>
       {todos
         ? todos.map((todoText) => {
             return <Todo todoText={todoText}></Todo>;
