9db5af9b6ec51d97ef828e65ed21617b54383824
andriiafanasiev <andreynoobroo@gmail.com>
2024-09-24 01:04:42 +0200

    Added better styles and React icons

--- src/components/Todos/Todo.jsx
+++ src/components/Todos/Todo.jsx
@@ -1,4 +1,6 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './Todo.module.css';
+
 function Todo({ todoText, index, deleteTodo }) {
   return (
     <div
@@ -8,11 +10,7 @@ function Todo({ todoText, index, deleteTodo }) {
       className={styles.todo}
     >
       <div className={styles.todo__info}>
-        <img
-          src="/assets/todo-icon.png"
-          alt="todo icon"
-          className={styles.todo__icon}
-        />
+        <RiTodoFill className={styles.todo__icon} />
         <p className={styles.todo__text}>{todoText}</p>
       </div>
     </div>
