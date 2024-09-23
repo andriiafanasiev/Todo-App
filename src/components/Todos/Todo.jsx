75931e6e522d0f5a2b0c89f059dab4170ec661b1
andriiafanasiev <andreynoobroo@gmail.com>
2024-09-23 20:08:07 +0200

    added a app icon

--- src/components/Todos/Todo.jsx
+++ src/components/Todos/Todo.jsx
@@ -2,7 +2,10 @@ import styles from './Todo.module.css';
 function Todo({ todoText }) {
   return (
     <div className={styles.todo}>
-      <div className={styles.todo__text}>{todoText}</div>
+      <div className={styles.todo__info}>
+        <img src="/assets/" alt="todo icon" className={styles.todo__icon} />
+        <p className={styles.todo__text}>{todoText}</p>
+      </div>
     </div>
   );
 }
