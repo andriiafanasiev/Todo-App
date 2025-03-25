28c63629aec5259f535bbb86651e0080806c4f7b
andriiafanasiev <andreynoobroo@gmail.com>
2025-03-25 23:02:15 +0100

    Added types to TodosActions

---
+++ src/components/Todos/TodosActions.tsx
@@ -0,0 +1,41 @@
+import Button from '../UI/Button';
+import styles from './TodosActions.module.css';
+import { MdDeleteForever } from 'react-icons/md';
+import { GrPowerReset } from 'react-icons/gr';
+
+interface TodosActionsProps {
+    resetTodos: () => void;
+    clearCompletedTodos: () => void;
+    completedTodosExist: () => void;
+}
+
+function TodosActions({
+    resetTodos,
+    clearCompletedTodos,
+    completedTodosExist,
+}: TodosActionsProps) {
+    return (
+        <div className={styles.todosActions__container}>
+            <Button
+                onClick={() => {
+                    resetTodos();
+                }}
+                title={'Reset Todos'}
+            >
+                <GrPowerReset />
+            </Button>
+
+            <Button
+                disabled={!completedTodosExist}
+                onClick={() => {
+                    clearCompletedTodos();
+                }}
+                title={'Clear completed todos'}
+            >
+                <MdDeleteForever />
+            </Button>
+        </div>
+    );
+}
+
+export default TodosActions;
